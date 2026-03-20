27dab751db2d15e344297c64c48952eadb8a4071
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2026-03-20 15:01:41 -0300

    validando o pedido

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/PedidoService.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/PedidoService.java
@@ -2,10 +2,13 @@ package io.github.cursodsousa.icompras.pedidos.service;
 
 import io.github.cursodsousa.icompras.pedidos.Repository.ItemPedidoRepository;
 import io.github.cursodsousa.icompras.pedidos.Repository.PedidoRepository;
+import io.github.cursodsousa.icompras.pedidos.client.ServicoBancarioClient;
 import io.github.cursodsousa.icompras.pedidos.model.Pedido;
 import io.github.cursodsousa.icompras.pedidos.validator.PedidoValidator;
+import jakarta.persistence.Transient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +17,23 @@ public class PedidoService {
     private final PedidoRepository repository;
     private final ItemPedidoRepository itemPedidoRepository;
     private final PedidoValidator validator;
+    private final ServicoBancarioClient servicoBancarioClient;
 
+    @Transactional
     public Pedido criarPedido(Pedido pedido){
+        validator.validar(pedido);
+        realizarPagamento(pedido);
+        enviarSolicitacaoPagamento(pedido);
+        return pedido;
+    }
+
+    private void realizarPagamento(Pedido pedido) {
         repository.save(pedido);
         itemPedidoRepository.saveAll(pedido.getItens());
-        return pedido;
+    }
+
+    private void enviarSolicitacaoPagamento(Pedido pedido) {
+        var chavePagamento = servicoBancarioClient.solicitarPagamento(pedido);
+        pedido.setChavePagamento(chavePagamento);
     }
 }
